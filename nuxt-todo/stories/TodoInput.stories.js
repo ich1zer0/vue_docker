2c576ea0be74d3f3d45f15db7a9af6948bf91628
徳永貴文 <proton.ich1@protonmail.com>
2021-02-11 18:12:41 +0900

    Add コンポーネントの story 追加

---
+++ nuxt-todo/stories/TodoInput.stories.js
@@ -0,0 +1,14 @@
+import TodoInput from '../components/TodoInput'
+
+export default {
+  title: 'Components/TodoInput',
+  components: TodoInput,
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { TodoInput },
+  template: '<TodoInput v-bind="$props" v-on="$props" />',
+})
+
+export const Default = Template.bind({})
